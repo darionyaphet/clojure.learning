609255cea0e50b1bf766278bca2ac2556d6e6e95
darionyaphet <darion.yaphet@gmail.com>
2015-05-22 21:44:47 +0800

    publish relation database

---
+++ common/relation-database/test/relation_database/core_test.clj
@@ -0,0 +1,7 @@
+(ns relation-database.core-test
+  (:require [clojure.test :refer :all]
+            [relation-database.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
