8b20c8f1fd5d6e77e1e69cee9c6ff5e3cf142e10
darionyaphet <darion.yaphet@gmail.com>
2015-11-15 23:59:29 +0800

    add data.json

---
+++ common/data.json/src/data/json.clj
@@ -0,0 +1,6 @@
+(ns data.json)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ common/data.json/test/data/json_test.clj
@@ -0,0 +1,7 @@
+(ns data.json-test
+  (:require [clojure.test :refer :all]
+            [data.json :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
